211e834a02306791770ffb5f95bcbe377e978eab
amotz taub tabib <amotz.tt@gmail.com>
2019-04-20 19:48:55 +0300

    some changes

--- linear search binary search on a list.py
+++ linear search binary search on a list.py
@@ -1,12 +1,12 @@
 import math
 
-
+DEVELOPMENT_MODE = True
 # this function takes two arguments, the first one is the list and the second one is an element to find in the list. it loop through the list checking every element if it is the element that i specified and if it is it prints the number is in the list and return TRUE
 # if it doesn't find the number it print "the number is not in the list and returns false
 def linear_search(list, number_to_search):
-    for i in range(len(list)):
-        if list[i] == number_to_search:
-            print("the number is in the list", list, number_to_search)
+    for i in list:
+        if i == number_to_search:
+            print("the number is in the list and the index in the list is", list.index(number_to_search))
             return True
     print("the number is not in the list")
     return False
@@ -16,7 +16,7 @@ def linear_search(list, number_to_search):
 # using linear_search function to search if each number in the list_of_numberstosearch variable is in thet testcase list.
 def test_linear_search():
     testcase = [4, 8, 9]
-    list_of_numbers_to_search = [8, 4, 9]
+    list_of_numbers_to_search = [8, 4, 9, 3]
     for number_to_search in range(len(list_of_numbers_to_search)):
         assert (linear_search(testcase, list_of_numbers_to_search[number_to_search]))
 
@@ -31,35 +31,34 @@ test_linear_search()
 # if the list is empty so to not have an error i included an if statement that returns false
 
 def __binary_search(list, start, end, number_to_search):
-    mid = (start + end) / 2
-    if not list:
-        print("the list is empty so the number is for sure not in the list")
-        return False
-    if start - end == 0 and list[math.floor(mid)] != number_to_search:
+    mid = math.floor((start + end) / 2)
+    if DEVELOPMENT_MODE:
+        assert (list == sorted(list), "Error! binary search should only be used on sorted lists.")
+    if end == start and list[mid] != number_to_search:
         print("the number is not in the list", list, number_to_search)
         return False
-    if list[math.floor(mid)] == number_to_search:
+    if list[mid] == number_to_search:
         print("the number is in the list", list, number_to_search)
         return True
-    elif list[math.floor(mid)] < number_to_search:
-        boolean = __binary_search(list, math.floor(mid) + 1, end, number_to_search)
-        return boolean
+    elif list[mid] < number_to_search:
+        return __binary_search(list, mid + 1, end, number_to_search)
+
     else:
-        boolean1 = __binary_search(list, start, math.floor(mid) - 1, number_to_search)
-        return boolean1
+        return __binary_search(list, start, math.floor(mid) - 1, number_to_search)
+
 
 
 # this is the public variable. first of all i sort the list with the sort of python and then i use thet privet function of binary search with the sorted_list, start and end-1 parameters. i get a boolean and return it to the function.
 def binary_search(list, number_to_search):
     sorted_list = sorted(list)
-    boolean = __binary_search(sorted_list, 0, len(sorted_list) - 1, number_to_search)
-    return boolean
+    return __binary_search(sorted_list, 0, len(sorted_list) - 1, number_to_search)
+
 
 
 # using a variable testcase that contains a list i created another variable list_of_numberstosearch that is a list of numberstosearch. i loop through them using binarysearch function to check if they all are True or not
 def test_binary_search():
-    testcase = [2, 0, 9, 8, 5, 4, 6, 3, 1]
-    list_of_numberstosearch = [2, 0, 9, 8, 5, 4, 8, 6, 3, 1]
+    testcase = [0, 1, 3, 4, 5, 6, 8, 9]
+    list_of_numberstosearch = [0, 1, 8, 5, 4, 8, 6, 3, 1, 10]
     for number_to_search in range(len(list_of_numberstosearch)):
         assert (binary_search(testcase, list_of_numberstosearch[number_to_search]))
 
